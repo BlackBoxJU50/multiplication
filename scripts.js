084cba347aed286fc2a6d482799adc1a1b215ac9
Hasib Ahmed Khan <ahmedhasib2224@gmail.com>
2024-09-14 00:03:30 +0600

    ploaded

---
+++ scripts.js
@@ -0,0 +1,36 @@
+/*const tablecon = document.getElementById('table');
+const element = document.createElement('p');
+element.innerText='Hello';
+//tablecon.appendChild(element);
+const id= document.createElement('td')
+id.innerText = '003';
+const namee= document.createElement('td');
+namee.innerText = 'Khan';
+const address= document.createElement('td')
+address.innerText = 'Dhaka';
+const row = document.createElement('tr');
+row.appendChild(id);
+row.appendChild(namee);
+row.appendChild(address);
+document.getElementById('tablebody').appendChild(row);
+row.appendChild(5);
+row.appendChild('*');
+row.appendChild(1);
+row.appendChild('=');
+row.appendChild(5);
+document.getElementById('tablebody').appendChild(row);*/
+
+const numberInput= document.getElementById('number');
+const table = document.getElementById('table');
+const submit = document.getElementById('submit');
+
+const reset = document.getElementById('reset');
+submit.addEventListener('click',function(){
+const num = parseInt(numberInput.value);
+
+
+
+
+
+})
+
